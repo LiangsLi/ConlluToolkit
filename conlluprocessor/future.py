9331120e01c6df8de2ec5782a67f8d210d107904
LiangsLi <poales@sina.com>
2020-04-11 18:27:50 +0800

    add future

---
+++ conlluprocessor/future.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# Created by li huayong on 2020/4/11
+
+
+def word_dependency2char_dependency(conllu_file_or_data, output_file):
+    raise NotImplementedError()
+
+
+def generate_raw_conllu(raw_sentences, output_file):
+    raise NotImplementedError()
+
+
+if __name__ == '__main__':
+    pass
