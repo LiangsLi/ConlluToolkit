a731f32c0bcba8e0cd58654ca3ef800c6a232a9b
LiangsLi <poales@sina.com>
2020-04-21 21:47:05 +0800

    修改import使得项目可以作为子模块放到其他的项目中

---
+++ ConlluToolkit/future.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+# Created by li huayong on 2020/4/11
+
+
+def word_dependency2char_dependency(conllu_file_or_data, output_file):
+    """
+    词语基本依存转化为字符级别依存（未实现）
+
+    Args:
+        conllu_file_or_data:
+        output_file:
+
+    Returns:
+
+    """
+    raise NotImplementedError()
+
+
+def generate_raw_conllu(raw_sentences, output_file):
+    """
+    将生语料转化为conllu格式（未实现）
+
+    Args:
+        raw_sentences:
+        output_file:
+
+    Returns:
+
+    """
+    raise NotImplementedError()
+
+
+if __name__ == '__main__':
+    pass
---
+++ ConlluToolkit/type.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# Created by li huayong on 2020/4/11
+from typing import Union, List
+from conllu import TokenList
+
+CONLLUData = List[TokenList]
+CONLLUDataOrPath = Union[str, CONLLUData]
+
+if __name__ == '__main__':
+    pass
