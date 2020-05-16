869dd952d7a1ce712075fe6c0132883370964c64
LiangsLi <poales@sina.com>
2020-05-16 23:22:14 +0800

    fix source_suffix

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -27,7 +27,11 @@ author = "Liangs Li"
 # The full version, including alpha/beta/rc tags
 release = "1.0"
 
-source_suffix = [".rst", ".md"]
+source_suffix = {
+    ".rst": "restructuredtext",
+    ".txt": "markdown",
+    ".md": "markdown",
+}
 
 # -- General configuration ---------------------------------------------------
 
